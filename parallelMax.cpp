9b789b0b2c70fba2120d26d82844fabd95b2bc67
danielrings <rings.daniel@gmail.com>
2014-11-17 21:07:54 -0500

    e

--- parallelMax.cpp
+++ parallelMax.cpp
@@ -41,7 +41,6 @@ int main(int argc, char** argv)
 	double start = omp_get_wtime();
 	#pragma omp parallel
 	{
-		cout << "hit1\n";
 		double lBuffer[LOCAL_BUFF_SIZE];
 		double lC = 0;
 		double lD = 0;
@@ -57,8 +56,7 @@ int main(int argc, char** argv)
 		bool lContinue = true;
 		while(lContinue)
 		{
-		
-			cout << "hit2\n";	
+			
 			// FOR DEBUGGING
 			debugCount++; 
 			if(debugCount == DEBUG_FREQ)
